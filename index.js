b1b4839f043de2a96539182faaf21c1920eb7da9
RishabhRaj2005 <rishabhrajfeb22@gmail.com>
2023-12-19 00:04:18 +0530

    Changelog

---
+++ index.js
@@ -0,0 +1,49 @@
+import express from "express"
+import bodyParser from "body-parser"
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+var title =[];
+var blog=[];
+app.get("/",(req,res)=>{
+    res.render("index.ejs",{t:title , b:blog});
+});
+
+app.get("/post",(req,res)=>{
+    res.render("post.ejs");
+});
+
+app.post("/post",(req,res)=>{
+    title.push(req.body["title"]);
+    title.push(req.body["post"]);
+    res.redirect("/");
+})
+
+app.get("/delete",(req,res)=>{
+    res.render("delete.ejs");
+});
+
+app.post("/delete",(req,res)=>{
+    var toDel = req.body["title"];
+    var f=0;
+    for(var i=0;i<(title.length-1);i++){
+        if(title[i]===toDel){
+            f=1;
+        }
+        if(f===1){
+            title[i]=title[i+1];
+        }
+    }
+    title.pop();
+    blog.pop();
+    res.redirect("/");
+})
+
+app.listen(port,()=>{
+    console.log(`The app is running on ${port}`);
+})
